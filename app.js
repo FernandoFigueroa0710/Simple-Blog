7619bfdb37c1b14d2c14d1fcf0ce6a063c9d1128
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-08 19:12:49 -0600

    middleware set up

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const flash = require("connect-flash");
+const session = require("express-session");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const moment = require("moment");
+const expressValidator = require("express-validator");
+
+//connection to mongodb via monk
+const mongo = require("mongodb");
+const db = require("monk")("localhost/nodeblog");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -14,10 +25,52 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// express session middelware
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+//express validator middleware
+app.use(
+  expressValidator({
+    errorFormatter: (param, msg, value) => {
+      let namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+//connect flash middleware
+app.use(require("connect-flash")());
+app.use((req, res, next) => {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+//make our db accesible to the router
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
