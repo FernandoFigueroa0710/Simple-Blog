14dbd0d2e224a956d56097a3307e53fa9422681d
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-11 21:25:39 -0600

    posts route set

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mongo = require("mongodb");
 const db = require("monk")("localhost/simpleblog");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 // make moment global to the entire app.
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
